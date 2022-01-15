584bec910dcddd24a507e01e5ee302daba6e7834
Malika Ansari <malikaansari91@gmail.com>
2022-01-15 22:56:45 +0530

    Added Dashboard Route

--- src/components/AppRoutes/index.jsx
+++ src/components/AppRoutes/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "../Dashboard";
 import Onboarding from "../Onboarding";
 function AppRoutes() {
---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Heading, Text } from "../../shared";
+
+const Dashboard = () => {
+  return (
+    <div className="container">
+      <div className="text-center mt-60">
+        <Heading level="1">Welcome to dashboard!!</Heading>
+        <Text type="body">You can always creat another workplace later.</Text>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
